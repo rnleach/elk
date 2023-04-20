137bbff9685f196410aaa754699b3652f42c978b
Ryan <rnleach@users.noreply.github.com>
2023-04-19 20:47:38 -0600

    Added more heap tests.

--- tests/heap_tests.c
+++ tests/heap_tests.c
@@ -1,13 +1,11 @@
 #include "test.h"
+#include <limits.h>
 /*-------------------------------------------------------------------------------------------------
  *
  *                                          ElkHeap
  *
  *-----------------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------------------------------
- *                                  Test ElkHeap Data Integrity
- *-----------------------------------------------------------------------------------------------*/
 static int
 int_priority(void *p)
 {
@@ -15,13 +13,16 @@ int_priority(void *p)
     return *v;
 }
 
+/*-------------------------------------------------------------------------------------------------
+ *                                  Test ElkHeap Data Integrity
+ *-----------------------------------------------------------------------------------------------*/
 static void
 elk_heap_test_data_integrity(void)
 {
     ElkHeap *heap = elk_heap_new(sizeof(int), int_priority, 3);
     ElkCode ret = ELK_CODE_SUCCESS;
 
-    for(int i = -300; i <= 400; ++i) {
+    for (int i = -300; i <= 400; ++i) {
         heap = elk_heap_insert(heap, &i, &ret);
         assert(!elk_is_error(ret));
     }
@@ -29,7 +30,7 @@ elk_heap_test_data_integrity(void)
     assert(elk_heap_count(heap) == 701);
 
     int val = 0;
-    for(int i = 400; i >= -300; --i) {
+    for (int i = 400; i >= -300; --i) {
         int const *top = elk_heap_peek(heap);
         assert(*top == i);
         heap = elk_heap_top(heap, &val, &ret);
@@ -37,6 +38,46 @@ elk_heap_test_data_integrity(void)
         assert(!elk_is_error(ret));
     }
 
+    assert(elk_heap_count(heap) == 0);
+
+    heap = elk_heap_top(heap, &val, &ret);
+    assert(ret == ELK_CODE_EMPTY);
+
+    int const *top = elk_heap_peek(heap);
+    assert(!top);
+
+    heap = elk_heap_free(heap);
+    assert(!heap);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ *                                  Test Bounded ElkHeap Data Integrity
+ *-----------------------------------------------------------------------------------------------*/
+static void
+elk_heap_bounded_test_data_integrity(void)
+{
+    ElkHeap *heap = elk_bounded_heap_new(sizeof(int), 30, int_priority, 5);
+    ElkCode ret = ELK_CODE_SUCCESS;
+
+    int i = -10;
+    while (ret == ELK_CODE_SUCCESS) {
+        heap = elk_heap_insert(heap, &i, &ret);
+        if (ret == ELK_CODE_SUCCESS)
+            ++i;
+    }
+
+    assert(i == 20);
+    assert(elk_heap_count(heap) == 30);
+
+    int val = INT_MAX;
+    int prev_val = INT_MAX;
+    ret = ELK_CODE_SUCCESS;
+    do {
+        assert(val <= prev_val);
+        heap = elk_heap_top(heap, &val, &ret);
+    } while (ret == ELK_CODE_SUCCESS);
+
+    assert(ret == ELK_CODE_EMPTY);
     assert(elk_heap_count(heap) == 0);
 
     heap = elk_heap_top(heap, &val, &ret);
@@ -52,4 +93,5 @@ void
 elk_heap_tests(void)
 {
     elk_heap_test_data_integrity();
+    elk_heap_bounded_test_data_integrity();
 }
